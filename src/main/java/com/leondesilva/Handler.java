fe3e2b1a7d9e63c1fceccebb9405475383bd7575
Leon De Silva <leond@zone24x7.com>
2019-09-13 17:54:45 +0530

    Initial implementation of the handler and executor.

---
+++ src/main/java/com/leondesilva/Handler.java
@@ -0,0 +1,35 @@
+package com.leondesilva;
+
+/**
+ * Interface to represent a handler in JCor.
+ * Handler is equal to a single node or processor in chain of responsibility design.
+ *
+ * @param <I>   the input object
+ * @param <S>   the status object
+ */
+public interface Handler<I, S> {
+
+    /**
+     * Method to evaluate whether the current handler is capable for processing the given input object.
+     *
+     * @param input the input object
+     * @return  true if processing should happen through the current handler, else returns false
+     */
+    boolean evaluate(I input);
+
+    /**
+     * Method to do the processing.
+     *
+     * @param input     the input object
+     * @param status    the status object keep track of the status of execution
+     */
+    void process(I input, S status);
+
+    /**
+     * Method to let the executor know whether the processing should stop from the current handler or not.
+     *
+     * @param status    the status object
+     * @return          true if processing should end from the current handler, false if processing should also go for next handler
+     */
+    boolean shouldExitChainAfterProcessing(S status);
+}
---
+++ src/main/java/com/leondesilva/JCorExecutor.java
@@ -0,0 +1,42 @@
+package com.leondesilva;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * JCor executor class to execute a list of handlers.
+ *
+ * @param <I>   the input object type
+ * @param <S>   the status object type
+ */
+public class JCorExecutor<I, S> {
+    private List<Handler<I, S>> handlerList = new LinkedList<>();
+
+    /**
+     * Method to add a handler to execution list
+     *
+     * @param handler the handler to add
+     */
+    public void addHandler(Handler<I, S> handler) {
+        handlerList.add(handler);
+    }
+
+    /**
+     * Method to execute the given list of handlers for a given input and status object.
+     *
+     * @param input     the input object
+     * @param status    the status object
+     */
+    public void execute(I input, S status) {
+
+        for (Handler<I, S> handler : handlerList) {
+            if (handler != null && handler.evaluate(input)) {
+                handler.process(input, status);
+
+                if (handler.shouldExitChainAfterProcessing(status)) {
+                    break;
+                }
+            }
+        }
+    }
+}
